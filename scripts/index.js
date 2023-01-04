8bd7b43b1fd4a7da786593814c3a19defe597276
martin laukmanis <laukmanis@medienachse.de>
2023-01-04 15:39:10 +0100

    folder

---
+++ scripts/index.js
@@ -0,0 +1,15 @@
+// var animation = bodymovin.loadAnimation({
+//   container: document.getElementById('bm'),
+//   renderer: 'svg',
+//   loop: true,
+//   autoplay: true,
+//   path: 'data.json'
+// })
+
+var animation = lottie.loadAnimation({
+  container: document.getElementById('carlos'),
+  renderer: 'png',
+  loop: true,
+  autoplay: true,
+  path: 'carlos.json'
+})
